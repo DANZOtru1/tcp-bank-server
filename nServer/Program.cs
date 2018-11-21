b7e490cc14b3d4f16fc845a06e0f8648a6e8e368
DANZOtru1 <dan9803@mail.ru>
2018-11-22 02:42:09 +0700

    Revert "second commit"
    This reverts commit 6a82c28adc97ebc2fa83ebafdffc32d909bb321f.

--- nServer/Program.cs
+++ nServer/Program.cs
@@ -5,11 +5,6 @@ namespace nServer
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            ServerObject server = new ServerObject();
-            Task task = Task.Run(new Action(server.Listen));
-            task.Wait();
-        }
+        
     }
 }
